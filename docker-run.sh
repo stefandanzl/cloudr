c6f63def92e5edb5ba8caa5f3f1ed5b9277e63fe
Stefan Danzl <stefan.danzl@live.de>
2023-11-27 12:31:53 +0100

    run-file

--- docker-run.sh
+++ docker-run.sh
@@ -1,8 +1,13 @@
 docker run -d \
 -p 5212:5212 \
---mount type=bind,source=/portainer/cloudr/conf.ini,target=/cloudr/conf.ini \
---mount type=bind,source=/portainer/cloudr/cloudr.db,target=/cloudr/cloudr.db \
+# --mount type=bind,source=/portainer/cloudr/conf.ini,target=/cloudr/conf.ini \
+# --mount type=bind,source=/portainer/cloudr/cloudr.db,target=/cloudr/cloudr.db \
+-v /portainer/cloudr/conf.ini:/cloudr/conf.ini \
+-v /portainer/cloudr/cloudr.db:/cloudr/cloudr.db \
 -v /portainer/cloudr/uploads:/cloudr/uploads \
 -v /portainer/cloudr/avatar:/cloudr/avatar \
+-v /portainer/cloudr/mount:/cloudr/mount \
+--name cloudr \
+--restart always \
 ghcr.io/stefandanzl/cloudr:latest
 
